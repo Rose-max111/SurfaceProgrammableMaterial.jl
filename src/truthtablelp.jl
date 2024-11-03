34f0d824025a744bf5ffb55462647356e1a42b45
GiggleLiu <cacate0129@gmail.com>
2024-11-04 02:01:02 +0800

    update

--- src/truthtablelp.jl
+++ src/truthtablelp.jl
@@ -1,13 +1,13 @@
 # let state1 - state2 = val
-function _make_constriant(state1::Integer, state2::Integer, total_atoms::Int)
+function _make_constriant(state1::Integer, state2::Integer, nspin::Int)
     takespin(b::Integer, i::Int) = 2 * readbit(b, i) - 1
     weights = Int[]
-    for i in 1:total_atoms
-        for j in i+1:total_atoms
+    for i in 1:nspin
+        for j in i+1:nspin
             push!(weights, takespin(state1, i) * takespin(state1, j) - takespin(state2, i) * takespin(state2, j))
         end
     end
-    for i in 1:total_atoms
+    for i in 1:nspin
         push!(weights, takespin(state1, i) - takespin(state2, i))
     end
     return weights
@@ -18,34 +18,39 @@ function set_value(configuration::Integer, ancilla_mask::Integer, ancilla_config
 end
 
 # subspins: the spins to implement the rule, the rest are ancilla
-function find_proper_model(::CellularAutomata1D{N}, total_atoms::Int, logical_spins) where N
-    @assert length(logical_spins) == 4 && total_atoms >= 4 "length(logical_spins) ($logical_spins) must be 4 and total_atoms ($total_atoms) must be at least 4"
-    mask_ancilla = bmask(Int, length(logical_spins)+1:total_atoms)
-    ground_states = collect(0:2^length(logical_spins)-1)
-    for ancilla_idx in 0:2^(total_atoms-length(logical_spins))-1
-        set_value.(ground_states, mask_ancilla, ancilla_idx << length(logical_spins))
-        res = find_ising_gadget(ground_states, setdiff(1:total_atoms, logical_spins), total_atoms, logical_spins, ((a, b, c, d)->automatarule(a, b, c, ruleid) == d))
+function find_proper_model(rule::CellularAutomata1D{N}, nspin::Int) where N
+    @assert nspin >= 4 "nspin ($nspin) must be at least 4"
+    nancilla = nspin - 4
+    ground_states0 = [b for b in 0:2^4-1 if rule(readbit(b, 1), readbit(b, 2), readbit(b, 3)) == readbit(b, 4)]
+    @assert length(ground_states0) == 8
+    for ancilla_idx in 0:256^nancilla-1  # ancilla configurations: e.g. for ancilla (a, b), the configurations are (b7,..., b1, b0, a7,..., a1, a0)
+        ground_states = copy(ground_states0)
+        for l = 1:8, k = 1:nancilla
+            ancilla_config = readbit(ancilla_idx, 8 * (k-1) + l)
+            ground_states[l] |= (ancilla_config << (3 + k - 1))
+        end
+        res = find_ising_gadget(ground_states, setdiff(0:2^nspin-1, ground_states), nspin)
         if res !== nothing
-            return mask, res
+            return ancilla_idx, res
         end
     end
     return -1, false
 end
 
-function find_ising_gadget(ground_states, excited_states, total_atoms, logical_spins, constraint)
-    @assert length(ground_states) + length(excited_states) == 2^total_atoms
+function find_ising_gadget(ground_states, excited_states, nspin)
+    @assert length(ground_states) + length(excited_states) == 2^nspin
     #model = @suppress Model(HiGHS.Optimizer)
     model = Model(HiGHS.Optimizer)
     set_silent(model)
-    @variable(model, x[1:Int(total_atoms * (total_atoms - 1) / 2 + total_atoms)])  # Jij and hi
+    @variable(model, x[1:Int(nspin * (nspin - 1) / 2 + nspin)])  # Jij and hi
     delta = 1
     for id in 2:length(ground_states)
-        weights = _make_constriant(ground_states[1], ground_states[id], total_atoms)
+        weights = _make_constriant(ground_states[1], ground_states[id], nspin)
         @constraint(model, sum(weights[i] * x[i] for i in 1:length(weights)) == 0)
     end
 
     for id in 1:length(excited_states)
-        weights = _make_constriant(ground_states[1], excited_states[id], total_atoms)
+        weights = _make_constriant(ground_states[1], excited_states[id], nspin)
         @constraint(model, sum(weights[i] * x[i] for i in 1:length(weights)) <= -delta)
     end
 
@@ -53,22 +58,17 @@ function find_ising_gadget(ground_states, excited_states, total_atoms, logical_s
     optimize!(model)
     if is_solved_and_feasible(model)
         return [value(x[i]) for i in 1:length(x)]
-    else
-        @info "constraint = $constraint, failed to find a solution"
-    end
-    for con in all_constraints(model; include_variable_in_set_constraints = false)
-        delete(model, con)
     end
     return nothing
 end
 
 
-function query_model(rule::CellularAutomata1D, total_atoms)
-    mask, weights = find_proper_model(rule, total_atoms, [1,2,3,4])
-    if mask == -1
+function query_model(rule::CellularAutomata1D, nspin)
+    ancilla_idx, weights = find_proper_model(rule, nspin)
+    if ancilla_idx == -1
         return -1, false
     end
     weights = round.(weights, digits=2)
     weights = weights ./ 0.25
-    return mask, weights
+    return [readbit(ancilla_idx, i) for i in 1:nspin-4], weights
 end
